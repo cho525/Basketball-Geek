a598950d0fb0e65e34510a1af69f6b414290957c
Charz <Charles@crc-dot1x-nat-10-239-19-129.bu.edu>
2023-05-05 21:33:24 -0400

    added super user for charles

--- interior/bball/gamestats/admin.py
+++ interior/bball/gamestats/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+# visit admin page, http://127.0.0.1:8000/admin
+# super user account and password
+# username: cho 
+# password: Charles525
+
+
